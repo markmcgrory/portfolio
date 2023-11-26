aa2ac4a7aacfddb528d1b7cfaa90a2b5ea5b46b5
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-11-26 11:31:24 +0000

    screenshots added to images folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return ( 
       <div className='home-container'>
         <div className='home-text'>
-          I'm <span className='bold-name'>Mark McGrory</span>, a budding software developer based in Shetland. Welcome to my portfolio site. Please take a look around.
+          I'm <span className='bold-name'>Mark McGrory</span>, a software developer based in Shetland. Welcome to my portfolio site. It's still under construction.
         </div>
       </div>
   );
--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -4,19 +4,22 @@ const Projects = [
 		id: 1, 
 		name: "Subway Samurai", 
 		image: '', 
-		github: ''
+		github: 'https://github.com/Dannycarey23/SubwaySamurai_frontend',
+		description: ''
     },
 	{
 		id: 2,
 		name: "Navigate App",
 		image: '',
-		gitHub: ''
+		gitub: 'https://github.com/markmcgrory/navigate_app',
+		description: ''
 	},
 	{
 		id: 3,
 		name: "Bookshop Manager",
 		image: '',
-		gitHib: ''
+		github: 'https://github.com/markmcgrory/python_project',
+		description: ''
 	}
 ]
 
