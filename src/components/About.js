2b96188ca13656c3b8e8c51326a925c490c852dd
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-12-05 14:24:02 +0000

    about text updated

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ const About = () => {
   return (
     <div className='about-container'>
       <div className='about-text'>
-        <p>I am a career changer with almost twenty years of varied work experience behind me, but my <strong>curiosity</strong>, capacity for <strong>problem solving</strong>, and <strong>passion for learning</strong> drove me to pursue a new path in software development.</p>
+        <p>I am a career changer with almost twenty years of varied work experience behind me, but my <strong>curiosity</strong>, capacity for <strong>problem solving</strong>, and <strong>love of learning</strong> drove me to pursue a new path in software development.</p>
 
-        <p>I'm a recent graduate of CodeClan's Professional Software Development bootcamp in Glasgow - as it happens, this was the last ever Glasgow cohort - where I learned to build <strong>full-stack</strong> applications using <strong>Python, JavaScript, Java, React</strong>, as well as a number of frameworks and database systems.</p>
+        <p>I'm a recent graduate of CodeClan's Professional Software Development bootcamp in Glasgow, where I learned to build <strong>full-stack</strong> applications using <strong>Python, JavaScript, Java & React</strong>, as well as using databases such as PostgreSQL and MongoDB and other technologies.</p>
 
-        <p>All aspects of the process are enjoyable and interesting to me, but my passion is for producing <strong>accessible, visually appealing applications</strong> that fulfill a genuine need. My love of photography has honed my eye for good composition, and that's something I bring to the table, as well as my love of working in teams, my excellent communication skills, and my ability to break down difficult concepts.</p>
+        <p>All aspects of the process are enjoyable to me, but I'm particularly interestd in producing <strong>accessible, visually appealing applications</strong> that fulfill a genuine need. My experience in photography has honed my eye for good composition, and that's something I bring to the table, as well as my love of working collaboratively and my excellent <strong>communication skills</strong></p>
 
-        <p>Having equipped myself with this solid groundwork of practical programming skills as well as a lived experience of the value of <strong>TDD, Agile practices, pair programming</strong>, etc., I am now looking for an opportunity to apply these skills commercially and to continue my journey as a developer.</p>
+        <p>I am now looking for an opportunity to apply these skills commercially and to continue my journey as a developer.</p>
       </div>
 
       {/* Include the image on the right */}
