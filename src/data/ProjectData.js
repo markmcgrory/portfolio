b142b3d3e31525521c86685f65483fdf38870ccb
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-12-05 13:27:53 +0000

    project list no longer colum

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -11,6 +11,7 @@ const Projects = [
 			'/images/subwayimage5.png',
 		],
 		github: 'https://github.com/Dannycarey23/SubwaySamurai_frontend',
+		shortDescription: 'Zombie game set in Glasgow',
 		description: 'Subway Samurai is a comic zombie brawler game set in Glasgow. It was created in a small group in just two weeks for our final CodeClan project. We incorporated music and sound effects, using Howler.js, as well as turn-based combat - with increasing difficulty levels as the player proceeds - as well as a health items utility and mega attacks.',
 		techStack: 'React, JavaScript, Java, Spring, Howler.js',
 		linkedin: 'https://www.linkedin.com/feed/update/urn:li:activity:7131990495580319745/'
@@ -23,6 +24,7 @@ const Projects = [
 			'/images/navigateimage2.png',
 		],
 		github: 'https://github.com/markmcgrory/navigate_app',
+		shortDescription: 'Homelessness app',
 		description: 'Navigate is an internal, location-data and management application for the social services. We wanted to create an app that could store service users and support services data, allowing the user to add, update and assign users to services around Glasgow. This was built in six days and formed our second CodeClan project.',
 		techStack: 'React, JavaScript, MongoDB, Express, ArcGIS'
 	},
@@ -34,6 +36,7 @@ const Projects = [
 			'/images/pythonimage2.png',
 		],
 		github: 'https://github.com/markmcgrory/python_project',
+		shortDescription: 'Bookstore app',
 		description: "Bookshop Manager was my first full-stack application, which I built from scratch in six days. It's aimed at staff working in a small bookstore. Functionality implemented: show book inventory, add to it or delete from it, show book details, update book details. Ditto with wholesalers, but the user can also filter books by wholesaler.",
 		techStack: 'Python, Flask, PostgreSQL'
 	}
