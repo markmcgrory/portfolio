8cfbf6b59b720dc1859368b2903419c48c24c6cc
Mark McGrory <mark_mcgrory@hotmail.co.uk>
2023-12-24 13:43:57 +0000

    footer react link added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
     return ( 
         <div className='footer'>
-            Mark McGrory 2023. Built using React.
+            Mark McGrory 2023. Built using&nbsp;<a href='https://react.dev/'>React</a>.
         </div>
      );
 }
